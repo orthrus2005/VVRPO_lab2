90cbe4eb305704d8948afa2212dff37981dd2032
Vatslav Gordynski <vat.mart2005@gmail.com>
2023-11-13 15:52:02 +0300

    2

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -13,6 +13,7 @@ int main() {
     for (int i = 0; i < size; i++) {
         arr[i] = rand() % 10    ; 
     }
+    //
 
     for (int i = 0; i < size; i++) {
         std::cout << "элемент " << arr[i] << std::endl;
